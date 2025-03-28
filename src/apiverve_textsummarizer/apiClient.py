9c80bc3a796bba80145ab4ce4735970e41bcd0c4
APIVerve Team <hello@apiverve.com>
2025-03-28 17:56:55 -0500

    Bug Fixes

--- src/apiverve_textsummarizer/apiClient.py
+++ src/apiverve_textsummarizer/apiClient.py
@@ -10,10 +10,10 @@ class TextsummarizerAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/textsummarizer'
+        self.base_url = 'https://api.apiverve.com/v1/textsummarizer'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
